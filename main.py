48e20e1d5eef85d14017b3bd2eddf05dba5bed13
kjfanj <kjfanj@gmail.com>
2019-05-06 23:36:19 -0500

    cleaned up and now working

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import webbrowser
 
-# url = 'http://www.python.org/'
-
-# # Open URL in a new tab, if a browser window is already open.
-# webbrowser.open_new_tab(url + 'doc/')
-
-# # Open URL in new window, raising the window if possible.
-# webbrowser.open_new(url)
-
 
 def get_websites(filename):
+    """
+    take in a .txt file and return each line of text in a list
+    """
     with open(filename) as f:
         read_data = f.read()
-        print(read_data)
         return read_data.split('\n')
 
 
+def openWebsites(websites):
+    """
+    opening the websites
+    """
+    for website in websites:
+        if website:
+            webbrowser.open_new(website)
+
+
 def main():
-    # run everything here
+        # get websites from txt file first
     websites = get_websites('websites.txt')
-    print(websites)
+
+    # open websites
+    openWebsites(websites)
 
 
 if __name__ == "__main__":
